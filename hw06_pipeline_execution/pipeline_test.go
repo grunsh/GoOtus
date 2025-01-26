666815af13f7b77c76bf71c903329a9f18cbe046
grunsh <alexander@grunin.ru>
2025-01-27 01:25:28 +0300

    Убрал свой тест

--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -158,68 +158,68 @@ func TestAllStageStop(t *testing.T) {
 	})
 }
 
-func TestTenStages(t *testing.T) {
-	fmt.Println(t.Name())
-	if !isFullTesting {
-		return
-	}
-	wg := sync.WaitGroup{}
-	// Stage generator
-	g := func(_ string, f func(v interface{}) interface{}) Stage {
-		return func(in In) Out {
-			out := make(Bi)
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				defer close(out)
-				for v := range in {
-					time.Sleep(sleepPerStage)
-					out <- f(v)
-				}
-			}()
-			return out
-		}
-	}
-
-	stages := []Stage{
-		g("Dummy", func(v interface{}) interface{} { return v }),
-		g("Multiplier (* 2)", func(v interface{}) interface{} { return v.(int) * 2 }),
-		g("Adder (+ 100)", func(v interface{}) interface{} { return v.(int) + 100 }),
-		g("Stringifier", func(v interface{}) interface{} { return strconv.Itoa(v.(int)) }),
-		g("Add1", func(v interface{}) interface{} { return v.(string) + "1" }),
-		g("Add2", func(v interface{}) interface{} { return v.(string) + "2" }),
-		g("Add3", func(v interface{}) interface{} { return v.(string) + "3" }),
-		g("Add4", func(v interface{}) interface{} { return v.(string) + "4" }),
-		g("Add5", func(v interface{}) interface{} { return v.(string) + "5" }),
-		g("Add6", func(v interface{}) interface{} { return v.(string) + "6" }),
-	}
-
-	t.Run("Ten stages", func(t *testing.T) {
-		fmt.Println(t.Name())
-		in := make(Bi)
-		done := make(Bi)
-		data := []int{1, 2, 3, 4, 5}
-
-		// Abort after 200ms
-		abortDur := sleepPerStage * 2
-		go func() {
-			<-time.After(abortDur)
-			close(done)
-		}()
-
-		go func() {
-			for _, v := range data {
-				in <- v
-			}
-			close(in)
-		}()
-
-		result := make([]string, 0, 10)
-		for s := range ExecutePipeline(in, done, stages...) {
-			result = append(result, s.(string))
-		}
-		wg.Wait()
-
-		require.Len(t, result, 0)
-	})
-}
+//func TestTenStages(t *testing.T) {
+//	fmt.Println(t.Name())
+//	if !isFullTesting {
+//		return
+//	}
+//	wg := sync.WaitGroup{}
+//	// Stage generator
+//	g := func(_ string, f func(v interface{}) interface{}) Stage {
+//		return func(in In) Out {
+//			out := make(Bi)
+//			wg.Add(1)
+//			go func() {
+//				defer wg.Done()
+//				defer close(out)
+//				for v := range in {
+//					time.Sleep(sleepPerStage)
+//					out <- f(v)
+//				}
+//			}()
+//			return out
+//		}
+//	}
+//
+//	stages := []Stage{
+//		g("Dummy", func(v interface{}) interface{} { return v }),
+//		g("Multiplier (* 2)", func(v interface{}) interface{} { return v.(int) * 2 }),
+//		g("Adder (+ 100)", func(v interface{}) interface{} { return v.(int) + 100 }),
+//		g("Stringifier", func(v interface{}) interface{} { return strconv.Itoa(v.(int)) }),
+//		g("Add1", func(v interface{}) interface{} { return v.(string) + "1" }),
+//		g("Add2", func(v interface{}) interface{} { return v.(string) + "2" }),
+//		g("Add3", func(v interface{}) interface{} { return v.(string) + "3" }),
+//		g("Add4", func(v interface{}) interface{} { return v.(string) + "4" }),
+//		g("Add5", func(v interface{}) interface{} { return v.(string) + "5" }),
+//		g("Add6", func(v interface{}) interface{} { return v.(string) + "6" }),
+//	}
+//
+//	t.Run("Ten stages", func(t *testing.T) {
+//		fmt.Println(t.Name())
+//		in := make(Bi)
+//		done := make(Bi)
+//		data := []int{1, 2, 3, 4, 5}
+//
+//		// Abort after 200ms
+//		abortDur := sleepPerStage * 2
+//		go func() {
+//			<-time.After(abortDur)
+//			close(done)
+//		}()
+//
+//		go func() {
+//			for _, v := range data {
+//				in <- v
+//			}
+//			close(in)
+//		}()
+//
+//		result := make([]string, 0, 10)
+//		for s := range ExecutePipeline(in, done, stages...) {
+//			result = append(result, s.(string))
+//		}
+//		wg.Wait()
+//
+//		require.Len(t, result, 0)
+//	})
+//}
