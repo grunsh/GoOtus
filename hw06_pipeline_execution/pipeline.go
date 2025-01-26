9ce58533b881d9d2b35651bf303ad21c31afa796
grunsh <alexander@grunin.ru>
2025-01-27 01:22:36 +0300

    Добавил тест. Почистил код

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,39 +8,35 @@ type (
 
 type Stage func(in In) (out Out)
 
-var I int
-
 func wrStage(in In, done In, s Stage) Out {
 	out := make(Bi)
 	var val interface{}
 	var ok bool
-	I++
-	go func(stageNum int) {
+	go func() {
 		defer close(out)
 		readChan := s(in)
-		//fmt.Println("Запущен стейдж: ", stageNum)
 		for {
 			select {
 			case <-done:
-				//fmt.Println("Выход из стейдж по done: ", stageNum)
 				for v := range readChan {
 					_ = v
 				}
 				return
 			case val, ok = <-readChan:
 				if !ok {
-					//fmt.Println("Выход из стейдж по закрытию канала чтения: ", stageNum)
 					return
 				}
 				select {
 				case out <- val:
 				case <-done:
-					//fmt.Println("Выход из стейдж по done2: ", stageNum)
+					for v := range readChan {
+						_ = v
+					}
 					return
 				}
 			}
 		}
-	}(I)
+	}()
 	return out
 }
 
