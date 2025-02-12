9ffdcbbcfb67c00b22dfddd27803e253d136d312
grunsh <alexander@grunin.ru>
2025-02-12 23:30:56 +0300

    Правки по тесту

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -17,7 +17,7 @@ func RunCmd(cmd []string, env Environment) (returnCode int) {
 		}
 		os.Setenv(k, v.Value)
 	}
-	fmt.Println("Команда с аргументами", cmd[2], cmd[3], cmd[4])
+	fmt.Println("Команда с аргументами", cmd[2], cmd[3], cmd[4], cmd[5])
 	comd := exec.Command(cmd[2], cmd[3], cmd[4]) //nolint
 	comd.Stdout = os.Stdout
 	comd.Stderr = os.Stderr
