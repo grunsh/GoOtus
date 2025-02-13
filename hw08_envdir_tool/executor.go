cf5ec02599a42ea1932f89d02d312ae27af3d8d8
grunsh <alexander@grunin.ru>
2025-02-13 11:45:33 +0300

    прошёл тест .sh

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -22,9 +22,10 @@ func RunCmd(cmd []string, env Environment) (returnCode int) {
 			exitCode = 1
 		}
 	}
-	comd := exec.Command(cmd[2], cmd[3], cmd[4]) //nolint
+	comd := exec.Command(cmd[2], cmd[3], cmd[4], cmd[5]) //nolint
 	comd.Stdout = os.Stdout
 	comd.Stderr = os.Stderr
+	comd.Stdin = os.Stdin
 	err := comd.Start()
 	if err != nil {
 		panic("Error starting command: " + err.Error())
